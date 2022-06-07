e09f2a28e36d74f977c5fb7794edcd469490135c
jarashi <jarashi.suki@gmail.com>
2022-06-07 16:24:07 +0900

    clean

--- src/WorldMap.jsx
+++ src/WorldMap.jsx
@@ -2,7 +2,6 @@ import * as topojson from 'topojson-client';
 import {
   geoOrthographic,
   geoPath,
-  geoGraticule,
   geoEquirectangular,
   geoCircle
 } from 'd3-geo';
@@ -13,26 +12,8 @@ import { useControls } from "leva"
 import useSVGDownload from './useSVGDownload';
 import { useRef } from 'react';
 
-const array = [...new Array(20)].map((_, i) => i%4);
-
-const split = (accumulator, currentValue, currentIndex, array) => {
-  if(currentIndex === 0) return "M20, 20"
-  let str = accumulator;
-  if(currentValue) str += ` ${currentValue ? "M" : "L"}${currentValue}`;
-  return str;//`${accumulator} ${currentValue ? "M" : "L"}10, 10`
-}
-
-
-function tester(projection) {
-  let visible;
-  const stream = projection.stream({point() { visible = true; }});
-  return ([x, y]) => (visible = false, stream.point(x, y), visible);
-}
-
-
 function Countries({ path }) {
   const world = topojson.feature(topo, topo.objects.countries);
-  console.log(world.features);
   const { color, bg, gap } = useControls({
     color: { value: "#9cbbcb" },
     bg: { value: "#2e4690" },
@@ -40,33 +21,22 @@ function Countries({ path }) {
   });
   return (
     <>
-    <path
-      d={path({"type": "Sphere"})}
-      fill={bg}
-    />
-    {world.features.map((feature, i) => (
-    <path
-      key={`map-feature-${i}`}
-      d={path(feature)}
-      fill={color}
-      stroke={bg}
-      strokeWidth={gap}
-      style={{ opacity: 0.5 }}
-    />
-  ))}
+      <path
+        d={path({"type": "Sphere"})}
+        fill={bg}
+      />
+      {world.features.map((feature, i) => (
+        <path
+          key={`map-feature-${i}`}
+          d={path(feature)}
+          fill={color}
+          stroke={bg}
+          strokeWidth={gap}
+          style={{ opacity: 0.5 }}
+        />
+      ))}
     </>
     
-  )
-}
-
-function Graticule({ path }) {
-  return (
-    <path
-      d={path(geoGraticule()())}
-      fill="transparent"
-      stroke="lightgray"
-      strokeWidth={0.5}
-    />
   )
 }
 
@@ -78,32 +48,17 @@ function Shade({ projection, date = new Date() }) {
 
   const path = geoPath().projection(projection);
   const night = geoCircle().radius(90).center(antipode([lng, lat]))
-  const sun = geoCircle().radius(90).center([lng, lat])
+  //const sun = geoCircle().radius(90).center([lng, lat])
 
   return (
-    <>
-     <path
+    <path
       d={path(night())}
-      //fill="rgba(0, 0, 0, .15)"
       fill="#1c1e2f51"
-      style={{
-        //mixBlendMode: "darken"
-      }}
-      />
-      <path
-      d={path(sun())}
-      //fill="rgba(0, 0, 0, .15)"
-      //fill="#397d8b44"
-      style={{
-        mixBlendMode: "screen"
-      }}
-      />
-    </>
-   
+    />
   )
 }
 
-function Grid({ projection, path }) {
+function Grid({ path }) {
   const { red } = useControls({
     red: { value: "#ff7878" }
   });
@@ -148,27 +103,7 @@ function Grid({ projection, path }) {
 }
 
 
-function Points({ projection, date = new Date() }) {
-
-  const points = [...Array(90)].map((_, i) => {
-    const { lat, lng } = getSatelliteLatLng(add(date, { minutes: i }))
-    return [lng, lat]
-  }).filter(tester(projection)).map(pt => {
-    return projection(pt)
-  })
-
-  return points.map((xy, i) => (
-    <circle
-      key={i}
-      cx={xy[0]}
-      cy={xy[1]}
-      fill="red"
-      r={2}
-    />
-  ))
-}
-
-function Lines({ projection, date = new Date(), path }) {
+function Lines({ date = new Date(), path }) {
   const points = [...Array(90)].map((_, i) => {
     const { lat, lng } = getSatelliteLatLng(add(date, { minutes: i }))
     return [lng, lat]
@@ -183,14 +118,12 @@ function Lines({ projection, date = new Date(), path }) {
     orbit: { value: "#fff" },
     width: { value: 2, min: 0, max: 2 }
   });
-  //console.log(path(line));
   return (
     <path
       d={path(line)}
       fill="none"
       stroke={orbit}
       strokeWidth={width}
-      //strokeDasharray={"4 4"}
     />
   )
 }
@@ -230,11 +163,6 @@ function EquirectangularMap({ width = 800, height = 400, date = new Date() }) {
   const scale = (width / 630) * 100;
   const translate = [centerX, centerY]
   const projection = geoEquirectangular().translate(translate).scale(scale)
-  //rotate([-135, -35, 0]).translate(translate).scale(scale*5).clipAngle(30)
-  //.center([135, 35])
-  //.clipExtent([[300,100], [500, 300]]).clipAngle(30)
-  //.clipExtent([[0,0], [90, 90]])
-  //.clipAngle(30);//.center([135, 35])
   const path = geoPath().projection(projection);
   const ref = useRef();
   useSVGDownload(ref)
